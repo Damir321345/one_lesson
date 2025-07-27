97a7a53e5abe24475d57c14cbdd2ad24e5e704f2
abdildanov_damir <d.abdildanov@bk.ru>
2025-07-27 22:19:10 +0500

    Промежуточный коммит

--- hello.go
+++ hello.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
-func Hello() string {
-	return "Hello world"
+const englHelloPregix = "Hello "
+
+func Hello(name string) string {
+	return englHelloPregix + name
 }
 
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("Любое значение"))
 }
