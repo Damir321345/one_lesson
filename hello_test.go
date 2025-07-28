903c8b43de3b5d08e0969a037452e736527a3de7
abdildanov_damir <d.abdildanov@bk.ru>
2025-07-28 09:20:58 +0500

    Дополненнный код с тестом

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello world"
+	got := Hello("Ran")
+	want := "Hello Ran"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
